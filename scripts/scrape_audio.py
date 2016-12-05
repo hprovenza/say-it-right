a13a2050d6b8992e3c9fb1ff2421e658d811a5ee
Hannah Provenza <hprovenza@gmail.com>
2016-12-04 22:11:04 -0500

    master transcription

---
+++ scripts/scrape_audio.py
@@ -0,0 +1,7 @@
+import urllib
+
+for x in range(1,64):
+    filename = "french" + str(x) + ".mp3"
+    print filename
+    u = urllib.URLopener()
+    u.retrieve("http://accent.gmu.edu/soundtracks/" + filename, filename)
