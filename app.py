e003557a469cf922671d06714af2fc5f58f64152
Hannah Provenza <hprovenza@gmail.com>
2016-12-14 10:18:43 -0500

    error correction for voiced th

--- app.py
+++ app.py
@@ -47,25 +47,17 @@ for i in range(len(correct)):
     s = SequenceMatcher(None, transcript[i], correct[i])
     #since opcodes returns a list you need to loop through inside
     for o in s.get_grouped_opcodes():
-        #i1, i2 are indexes of transcript; j1, j2 are indexes of correct
-        # for tag, i1, i2, j1, j2 in o:
-        #     #print(tag, i1, i2, j1, j2)
-        #    # print(tag, int(i1), int(i2), transcript[i][i1:i2], int(j1), int(j2), correct[i][j1:j2])
-        #     if tag == 'replace':
-        #         print(tag, int(i1), int(i2), transcript[i][i1:i2], int(j1), int(j2), correct[i][j1:j2])
-        #         if transcript[i][i1:i2] in devoice:
-        #             if devoice[transcript[i][i1:i2]] == correct[i][j1:j2]:
-        #                 #This is where the code should be to call the feedback
-        #                 print("you should voice your sound")
-        #             else:
-        #                 print("you are doing fine")
         for code in o:
             if code[0] == 'replace':
                 print(code)
                 if transcript[i][code[1]] in th_changes:
                     if (th_changes[transcript[i][code[1]:code[2]]] == correct[i][code[3]:code[4]]):
-                        print("th articulation error! at char {}".format(code[3]))
+                        print("unvoiced th articulation error! at char {}".format(code[3]))
                         mistake_reporting.th_articulation()
+                if transcript[i][code[1]] in dh_changes:
+                    if (dh_changes[transcript[i][code[1]:code[2]]] == correct[i][code[3]:code[4]]):
+                        print("voiced th articulation error! at char {}".format(code[3]))
+                        mistake_reporting.dh_articulation()
                 if transcript[i][code[1]] in devoice:
                     if (devoice[transcript[i][code[1]:code[2]]] == correct[i][code[3]:code[4]]):
                         print("devoicing error! at char {}".format(code[3]))
