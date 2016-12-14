6498623bb6e726b04776f9aca8c4d392bb7042f1
Hannah Provenza <hprovenza@gmail.com>
2016-12-13 21:08:45 -0500

    added basic correction output

---
+++ mistake_reporting.py
@@ -0,0 +1,10 @@
+
+
+def dh_articulation():
+    print "To make a voiced \"th\" sound, imagine making a \"z\" while sticking the tip of your tongue past your front teeth."
+
+def th_articulation():
+    print "To make an unvoiced \"th\" sound, imagine making an \"s\" while sticking the tip of your tongue past your front teeth."
+
+def final_devoicing():
+    print "In English, the \"s\" at the end of most plurals is usually pronounced like a \"z\" unles the sound before it is also devoiced."
