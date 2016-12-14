fe1bd73a65970a258015ea9c080708be321bf0f9
Hannah Provenza <hprovenza@gmail.com>
2016-12-13 20:28:30 -0500

    basic plan of attack added to app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import sys
+
+audio = sys.argv[]
+
+#TODO: script to get the phonetic transcription
+
+#TODO: align the transcription and master transcription
+
+#TODO: identify the differences
+
+#TODO: return the errors
