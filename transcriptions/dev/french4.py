d7586740e8f9bc130ca151ee1f7d3d8c99d63130
Hannah Provenza <hprovenza@gmail.com>
2016-12-14 07:12:00 -0500

    added manual transcriptions

---
+++ transcriptions/dev/french4.py
@@ -0,0 +1,3 @@
+priz pliz kQl stEl@ h{sk h@r tu brIN diz TINk wIf h@r fr@m D@ stOr sIks spun @v frES sno piz fayv tIk sl{bz @v blu
+tSiz {nd mebi @ sn{k fOr h@r br@d@r bAb wi Qlso nid @ smQl pl{stIk @ smAl pl{stik snek {nd @ bIg tOy frAg fOr d@ kIdz Si k{n skup
+diz fINk Intu tri rEd b{gz {nd wi wIl go mit h@r wEnzdi {t d@ tren steS@n
